ccb0bc4542671d232e45e67a8d39e7ce0e6eca86
AmyMcMahon1 <amy@provizio.ai>
2023-11-28 17:01:05 +0100

    set model

--- tests/test_yolov8trt.py
+++ tests/test_yolov8trt.py
@@ -50,14 +50,12 @@ class TestYolov8TRTDetectionModel(unittest.TestCase):
 
         download_yolov8_trt_model()
 
-        # with open(Yolov8TRTTestConstants.YOLOV8N_TRT_MODEL_PATH, 'rb') as f:
-        #     engine_data = f.read()
-        # yolo_model = self.runtime.deserialize_cuda_engine(engine_data)
-
-        # print(yolo_model)
+        with open(Yolov8TRTTestConstants.YOLOV8N_TRT_MODEL_PATH, 'rb') as f:
+            engine_data = f.read()
+        yolo_model = self.runtime.deserialize_cuda_engine(engine_data)
 
         yolov8_trt_detection_model = Yolov8TrtDetectionModel(
-            model=Yolov8TRTTestConstants.YOLOV8N_TRT_MODEL_PATH,
+            model_path=yolo_model,
             confidence_threshold=CONFIDENCE_THRESHOLD,
             iou_threshold=IOU_THRESHOLD,
             device=MODEL_DEVICE,
